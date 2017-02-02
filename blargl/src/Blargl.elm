eba0ae504f68f1e997c04ba4e87c52b21b729dd1
hellerve <veit.heller@student.htw-berlin.de>
2017-02-02 16:08:16 +0100

    no inline css in blargl; fixed print style

--- blargl/src/Blargl.elm
+++ blargl/src/Blargl.elm
@@ -1,59 +1,25 @@
-port module Blargl exposing (main, click)
+module Blargl exposing (main)
 
-import Html exposing (div, textarea, program, text, button)
-import Html.Attributes exposing (style, value)
-import Html.Events exposing (onInput, onClick)
+import Html exposing (div, textarea, beginnerProgram, text, button)
+import Html.Attributes exposing (style, value, class, id)
+import Html.Events exposing (onInput)
 import Markdown exposing (toHtml)
 
-type Msg = Str String | Click
+type Msg = Str String
 
-port click : String -> Cmd msg
-
-init = ("", Cmd.none)
-
-update msg i =
+update msg _ =
   case msg of
-    Str s -> (s, Cmd.none)
-    Click -> (i, click i)
+    Str s -> s
 
-
-input extra =
-  style ([ ("width",  "44vw")
-         , ("margin-left", "2.5vw")
-         , ("margin-right", "2.5vw")
-         , ("height", "80vh")
-         , ("margin-top", "5vh")
-         , ("border", "none")
-         , ("background-color", "white")
-         , ("resize", "none")
-         , ("float", "left")
-         , ("padding", "1em")
-         , ("box-sizing", "border-box")
-         , ("margin-bottom", "1em")
-         , ("font-size", "20px")
-         , ("overflow", "scroll")
-         ] ++ extra)
-
-btn = style [ ("border", "3px solid #333")
-            , ("border-radius", "0px")
-            , ("padding", "5px")
-            , ("font-size", "20px")
-            , ("margin-left", "90vw")
-            ]
-
-bg = style [ ("background-color", "#eee")
-           , ("height", "100vh")
-           ]
 
 view model =
-  div [ bg ] [
+  div [ class "bg" ] [
       textarea [ value model
                , onInput Str
-               , input []
+               , class "input"
                ] []
-    , toHtml [input [("font-family", "charter")]] model
-    , button [btn, onClick Click] [text "save"]
+    , toHtml [class "input", id "output"] model
   ]
 
 
-main = program { init=init, update=update, view=view, subscriptions=\x->Sub.none }
+main = beginnerProgram { model="", update=update, view=view }
