db5db065de105b9826068892e0fb86fd3b48fdfe
hellerve <veit@veitheller.de>
2020-03-18 13:37:04 +0100

    ssg: rewrite in haskell

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 The articles on my personal blog and the tools that power them.
 
-This repository includes a [dead-simple static site generator](https://github.com/hellerve/blog/blob/master/publish.zp)
-written in Python, a [distraction-free WYSIWYG Markdown editor](https://github.com/hellerve/blog/tree/master/blargl)
+This repository includes a [dead-simple static site generator](https://github.com/hellerve/blog/blob/master/publish.hs)
+written in Haskell, a [distraction-free WYSIWYG Markdown editor](https://github.com/hellerve/blog/tree/master/blargl)
 written in Elm, and a [RSS-feed generator and twitter bot (from `index.html`)](https://github.com/hellerve/blog/tree/master/rsser).
 The generators are tailored to my specific needs, but the Markdown
 editor could be useful for more people. [See it in action here](http://blog.veitheller.de/blargl).
---
+++ publish.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Data.DateTime (getCurrentTime, formatDateTime)
+import System.Environment (getArgs)
+import System.FilePath    (takeBaseName)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Lazy.Builder as TB
+import qualified Data.Text.Lazy.Builder.Int as TB
+
+import Text.Mustache (ToMustache(..), (~>), object, substitute, localAutomaticCompile)
+import Text.Pandoc   (def, readMarkdown, writeHtml5String, runIOorExplode)
+
+outFile = flip (++) ".html" . takeBaseName
+
+toTitle = map (\c -> if c == '_' then ' ' else c) . takeBaseName
+
+mdToHtml contents =
+  runIOorExplode
+    (readMarkdown def contents >>= writeHtml5String def)
+
+data Args = Args { post :: T.Text, title :: String, date :: String }
+
+instance ToMustache Args where
+  toMustache args = object
+      [ "post" ~> post args
+      , "title" ~> title args
+      , "date" ~> date args
+      ]
+
+makeArgs name contents = do
+  date <- getCurrentTime
+  post <- mdToHtml contents
+  return (Args { post=post
+               , title=toTitle name
+               , date=formatDateTime "%m/%d/%Y" date
+               })
+
+render target contents = do
+  compiled <- localAutomaticCompile "layout.html"
+  case compiled of
+    Left err -> error (show err)
+    Right template -> do
+      args <- makeArgs target contents
+      return (substitute template args)
+
+main = do
+  args <- getArgs
+  if length args /= 1
+  then error "Please provide a target to render."
+  else do
+    let target = head args
+    contents <- T.readFile target
+    rendered <- render target contents
+    T.writeFile (outFile target) rendered
